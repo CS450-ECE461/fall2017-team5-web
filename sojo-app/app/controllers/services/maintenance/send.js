0acc1124c1554ba10a591836d4339f5218960015
Kyle Peeler <kylepeeler@gmail.com>
2017-11-27 00:40:38 -0500

    added initial routes for different types of maintenance requests and message landlord

---
+++ sojo-app/app/controllers/services/maintenance/send.js
@@ -0,0 +1,8 @@
+import Controller from '@ember/controller';
+import Ember from 'ember';
+
+
+export default Controller.extend({
+  queryParams: ['type'],
+  type: 'Maintenance Request'
+});
---
+++ sojo-app/tests/unit/routes/services/maintenance/send-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:services/maintenance/send', 'Unit | Route | services/maintenance/send', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
