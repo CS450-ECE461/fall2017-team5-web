544c63cfe4231c16a5bb0d75f65cdb5e93af51ac
Kyle Peeler <kylepeeler@gmail.com>
2017-11-27 23:22:20 -0500

    added all services routes and UI, need to implement request

--- sojo-app/app/controllers/services/maintenance/send.js
+++ sojo-app/app/controllers/services/maintenance/send.js
@@ -4,5 +4,13 @@ import Ember from 'ember';
 
 export default Controller.extend({
   queryParams: ['type'],
-  type: 'Maintenance Request'
+  type: 'Maintenance Request',
+  messageCount: 300,
+  actions: {
+    updateMessageCount() {
+      var text_length = $('#landlord-message').val().length;
+      var text_remaining = 300 - text_length;
+      this.set('messageCount', text_remaining);
+    }
+  }
 });
