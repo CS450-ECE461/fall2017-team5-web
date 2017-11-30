f17f7a378a034ae0bb2ef72ab6a8e567f5beaa58
Kyle Peeler <kylepeeler@gmail.com>
2017-11-30 05:49:59 -0500

    dashboard clock shows correct day in correct position

--- sojo-app/app/components/dashboard-clock.js
+++ sojo-app/app/components/dashboard-clock.js
@@ -1,4 +1,6 @@
 import Ember from 'ember';
-
+var months = ["January", "Feburary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 export default Ember.Component.extend({
+  day: new Date().getDate(),
+  month: months[new Date().getMonth()]
 });
