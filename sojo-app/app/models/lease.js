455cf0adb3eb51c444aadb619bb5422fc906aaae
Kyle Peeler <kylepeeler@gmail.com>
2017-12-02 20:34:11 -0500

    added lease type

--- sojo-app/app/models/lease.js
+++ sojo-app/app/models/lease.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   rent_amount: DS.attr('number'),
+  lease_type: DS.attr('string'),
   start_date: DS.attr('date'),
   end_date: DS.attr('date'),
   account_id: DS.belongsTo('account'),
