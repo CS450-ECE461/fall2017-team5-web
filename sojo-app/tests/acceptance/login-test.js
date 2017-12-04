843158d69ca0924e017ed6d5b1bd9fea46b300fd
Nick <namonroe@iupui.edu>
2017-12-04 10:05:45 -0500

    Created index and login acceptance tests

---
+++ sojo-app/tests/acceptance/login-test.js
@@ -0,0 +1,12 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'sojo-app/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | login');
+
+test('visiting /login', function(assert) {
+  visit('/login');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/login');
+  });
+});
